6c4b694e1b78d3fa977aa6a289ee0e32f5e7b7d8
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2022-06-30 14:13:09 +0400

    Add workshop starter point

---
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/main/view/ui/activity/WorkshopActivity.kt
@@ -0,0 +1,13 @@
+package org.hyperskill.app.android.main.view.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.hyperskill.app.android.R
+
+class WorkshopActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workshop)
+    }
+}
